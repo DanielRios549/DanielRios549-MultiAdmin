3fbaf363a98891496d93df7afe9b5d4791e819d0
Daniel Rios <danielrios.flamengo@gmail.com>
2021-11-21 02:44:58 -0300

    Add SVG support

--- src/parts/header.svelte
+++ src/parts/header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import {handleTheme} from '../stores/theme'
+    import SVG from 'svelte-inline-svg'
 </script>
 
 <header>
     <button on:click={handleTheme}>Change Theme</button>
+    <SVG src="/icons/dark.svg"/>
     <menu>
         <ul>
             <li>Profile</li>
