77b26b2d8a6d6daf6da725cb36494632b570c290
Daniel Rios <danielrios.flamengo@gmail.com>
2021-11-21 00:25:26 -0300

    Update Dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess({
+		sourceMap: true
+	}),
 	kit: {
 		target: '#app'
 	}
